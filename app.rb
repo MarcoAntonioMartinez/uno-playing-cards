4be7542293fe856f9c351637cf0c99995352dc1d
Marco Martinez <marco.antoni.martinez@gmail.com>
2025-08-26 01:33:48 +0000

    Removed post actions

--- app.rb
+++ app.rb
@@ -47,36 +47,3 @@ resp = HTTP.get(start_game)
 
 end
 
-
-# The POST route that receives the data from JavaScript
-post ("/api/process-click") do
-
-  json = JSON.parse(request.body.read)
-  console.log(json)
-
-  # Access the data sent from the client via the `params` hash.
-  clicked_item = params['item']
-
-  # Process the data (e.g., log it or save it to a database).
-  console.log("Received click for item: #{clicked_item}")
-
-  # Send a JSON response back to the client.
-  content_type :json
-  { message: "Server received: '#{clicked_item}'" }.to_json
-end
-
-post ("/game") do
-
-  json = JSON.parse(request.body.read)
-  console.log(json)
-
-  # Access the data sent from the client via the `params` hash.
-  clicked_item = params['item']
-
-  # Process the data (e.g., log it or save it to a database).
-  console.log("Received click for item: #{clicked_item}")
-
-  # Send a JSON response back to the client.
-  content_type :json
-  { message: "Server received: '#{clicked_item}'" }.to_json
-end
